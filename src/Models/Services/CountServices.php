8faa4dd88de71e77c3bf7b226891285855845363
woisks <woisks@126.com>
2019-08-01 19:38:01 +0800

    remove countServices logs

--- src/Models/Services/CountServices.php
+++ src/Models/Services/CountServices.php
@@ -18,7 +18,6 @@ namespace Woisks\Count\Models\Services;
 use DB;
 use Throwable;
 use Woisks\Count\Models\Repository\CountRepository;
-use Woisks\Count\Models\Repository\LogRepository;
 use Woisks\Count\Models\Repository\TypeRepository;
 
 /**
@@ -45,58 +44,46 @@ class CountServices
      */
     private static $typeRepo;
 
-    /**
-     * logRepo.  2019/8/1 18:10.
-     *
-     * @var static LogRepository
-     */
-    private static $logRepo;
-
 
     /**
      * CountServices constructor. 2019/8/1 18:09.
      *
      * @param CountRepository $countRepo
      * @param TypeRepository $typeRepo
-     * @param LogRepository $logRepo
      *
      * @return void
      */
-    public function __construct(CountRepository $countRepo, TypeRepository $typeRepo, LogRepository $logRepo)
+    public function __construct(CountRepository $countRepo, TypeRepository $typeRepo)
     {
         self::$countRepo = $countRepo;
         self::$typeRepo  = $typeRepo;
-        self::$logRepo   = $logRepo;
+
 
     }
 
 
     /**
-     * increment. 2019/8/1 18:13.
+     * increment. 2019/8/1 19:37.
      *
      * @param $model
      * @param $type
      * @param $numeric
-     * @param $account_uid
      *
      * @return bool
      * @throws \Exception
      */
-    public static function increment($model, $type, $numeric, $account_uid)
+    public static function increment($model, $type, $numeric)
     {
         if (!$type_db = self::$typeRepo->first($model, $type)) {
             return false;
         }
 
-        if (self::$logRepo->exists($model, $type, $numeric, $account_uid)) {
-            return true;
-        }
         try {
             DB::beginTransaction();
 
             $type_db->increment('count');
             self::$countRepo->firstOrCreated($model, $type, $numeric);
-            self::$logRepo->created($model, $type, $numeric, $account_uid);
+
         } catch (Throwable $e) {
 
             DB::rollBack();
@@ -109,23 +96,18 @@ class CountServices
 
 
     /**
-     * decrement. 2019/8/1 18:13.
+     * decrement. 2019/8/1 19:37.
      *
      * @param $model
      * @param $type
      * @param $numeric
-     * @param $account_uid
      *
      * @return bool
      * @throws \Exception
      */
-    public static function decrement($model, $type, $numeric, $account_uid)
+    public static function decrement($model, $type, $numeric)
     {
         if (!$type_db = self::$typeRepo->first($model, $type)) {
-            return false;
-        }
-
-        if (!self::$logRepo->exists($model, $type, $numeric, $account_uid)) {
             return false;
         }
 
@@ -134,7 +116,7 @@ class CountServices
 
             $type_db->decrement('count');
             self::$countRepo->decrement($model, $type, $numeric);
-            self::$logRepo->delete($model, $type, $numeric, $account_uid);
+
         } catch (\Throwable $e) {
 
             \DB::rollBack();
